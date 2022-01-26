5b14b1e18070e769b0836e2d08cd9056c2f99d79
UT0P1C <ljonham@gmail.com>
2022-01-26 20:34:54 -0300

    adicionando comentarios

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+
+//função para inserir os numeros do HTML 
+
 function insert(num)
 {
     var numero = document.getElementById('result').innerHTML;
     document.getElementById('result').innerHTML = numero + num;
 }
 
+//função para limpar todos os numeros inseridos
+
 function clean()
 {
     document.getElementById('result').innerHTML = "";
 }
+
+//Função para apagar o ultimo numero inserido
 
 function back()
 {
@@ -15,6 +22,7 @@ function back()
     document.getElementById('result').innerHTML = result.substring(0, result.length - 1 );
 }
 
+//Função para calcular
 
 function calc(){
     var result = document.getElementById('result').innerHTML;
